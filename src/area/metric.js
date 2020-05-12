107e120642ccc814bb9df40ef6a7a6e9a3453dab
Shane Connor <shanetconnor@gmail.com>
2020-05-11 21:09:09 -0700

    updated metric areas functions and test. Tests are at 100% coverage.

--- src/area/metric.js
+++ src/area/metric.js
@@ -4,9 +4,19 @@ import distance from '../distance';
 const {
   millimeterToCentimeter,
   millimeterToMeter,
-  centimeterToMillimeter
+  millimeterToKilometer,
+  centimeterToMillimeter,
+  centimeterToMeter,
+  centimeterToKilometer,
+  meterToMillimeter,
+  meterToCentimeter,
+  meterToKilometer,
+  kilometerToMillimeter,
+  kilometerToCentimeter,
+  kilometerToMeter
 } = distance.metric;
 
+// Millimeter to ...
 const squareMillimeter = (width, height = width) => { 
   return formulas.squareArea(width, height);
 };
@@ -23,18 +33,98 @@ const squareMillimeterToMeter = (width, height = width) => {
   return formulas.squareArea(mWidth, mHeight);
 };
 
+const squareMillimeterToKilometer = (width, height = width) => {
+  const mWidth = millimeterToKilometer(width);
+  const mHeight = millimeterToKilometer(height);
+  return formulas.squareArea(mWidth, mHeight);
+};
+
+// Centimeter to ...
+const squareCentimeter = (width, height = width) => { 
+  return formulas.squareArea(width, height);
+};
+
 const squareCentimeterToMillimeter = (width, height = width) => {
   const cmWidth = centimeterToMillimeter(width);
   const cmHeight = centimeterToMillimeter(height);
   return formulas.squareArea(cmWidth, cmHeight);
+};
 
+const squareCentimeterToMeter = (width, height = width) => {
+  const cmWidth = centimeterToMeter(width);
+  const cmHeight = centimeterToMeter(height);
+  return formulas.squareArea(cmWidth, cmHeight);
+};
+
+const squareCentimeterToKilometer = (width, height = width) => {
+  const cmWidth = centimeterToKilometer(width);
+  const cmHeight = centimeterToKilometer(height);
+  return formulas.squareArea(cmWidth, cmHeight);
+};
+
+// Meter to ...
+const squareMeter = (width, height = width) => { 
+  return formulas.squareArea(width, height);
+};
+
+const squareMeterToMillimeter = (width, height = width) => {
+  const cmWidth = meterToMillimeter(width);
+  const cmHeight = meterToMillimeter(height);
+  return formulas.squareArea(cmWidth, cmHeight);
+};
+
+const squareMeterToCentimeter = (width, height = width) => {
+  const cmWidth = meterToCentimeter(width);
+  const cmHeight = meterToCentimeter(height);
+  return formulas.squareArea(cmWidth, cmHeight);
+};
+
+const squareMeterToKilometer = (width, height = width) => {
+  const cmWidth = meterToKilometer(width);
+  const cmHeight = meterToKilometer(height);
+  return formulas.squareArea(cmWidth, cmHeight);
+};
+
+// Kilometer to ...
+const squareKilometer = (width, height = width) => { 
+  return formulas.squareArea(width, height);
+};
+
+const squareKilometerToMillimeter = (width, height = width) => {
+  const cmWidth = kilometerToMillimeter(width);
+  const cmHeight = kilometerToMillimeter(height);
+  return formulas.squareArea(cmWidth, cmHeight);
+};
+
+const squareKilometerToCentimeter = (width, height = width) => {
+  const cmWidth = kilometerToCentimeter(width);
+  const cmHeight = kilometerToCentimeter(height);
+  return formulas.squareArea(cmWidth, cmHeight);
+};
+
+const squareKilometerToMeter = (width, height = width) => {
+  const cmWidth = kilometerToMeter(width);
+  const cmHeight = kilometerToMeter(height);
+  return formulas.squareArea(cmWidth, cmHeight);
 };
 
 const metric = {
   squareMillimeter,
   squareMillimeterToCentimeter,
   squareMillimeterToMeter,
-  squareCentimeterToMillimeter
+  squareMillimeterToKilometer,
+  squareCentimeter,
+  squareCentimeterToMillimeter,
+  squareCentimeterToMeter,
+  squareCentimeterToKilometer,
+  squareMeter,
+  squareMeterToMillimeter,
+  squareMeterToCentimeter,
+  squareMeterToKilometer,
+  squareKilometer,
+  squareKilometerToMillimeter,
+  squareKilometerToCentimeter,
+  squareKilometerToMeter
 };
 
 export default metric;
--- tests/area/metric.spec.js
+++ tests/area/metric.spec.js
@@ -1,25 +1,139 @@
 import metric from '../../src/area/metric';
 
-test('should convert square millimeter test 1', () => {
-  expect(metric.squareMillimeter(4, 4)).toBe(16);
+// Millimeter to ...
+describe('Millimeter to ...', () => { 
+  test('should convert square millimeter test 1', () => {
+    expect(metric.squareMillimeter(4, 4)).toBe(16);
+  });
+  
+  test('should convert square millimeter test 2', () => {
+    expect(metric.squareMillimeter(5)).toBe(25);
+  });
+  
+  test('should convert square millimeter to centimeter test 1', () => {
+    expect(metric.squareMillimeterToCentimeter(40, 40)).toBe(16);
+  });
+  
+  test('should convert square millimeter to centimeter test 2', () => {
+    expect(metric.squareMillimeterToCentimeter(40)).toBe(16);
+  });
+  
+  test('should convert square millimeter to meter test 1', () => {
+    expect(metric.squareMillimeterToMeter(4000, 4000)).toBe(16);
+  });
+
+  test('should convert square millimeter to meter test 2', () => {
+    expect(metric.squareMillimeterToMeter(3000)).toBe(9);
+  });
+  
+  test('should convert square millimeter to kilometer test 1', () => {
+    expect(metric.squareMillimeterToKilometer(40000, 40000)).toBe(0.0016);
+  });
+  test('should convert square millimeter to kilometer test 2', () => {
+    expect(metric.squareMillimeterToKilometer(4000000)).toBe(16);
+  });
 });
 
-test('should convert square millimeter test 2', () => {
-  expect(metric.squareMillimeter(5)).toBe(25);
+// Centimeter to ...
+describe('Centimeter to ...', () => {
+  test('should convert square centimeter test 1', () => {
+    expect(metric.squareCentimeter(4, 4)).toBe(16);
+  });
+
+  test('should convert square centimeter test 2', () => {
+    expect(metric.squareCentimeter(5)).toBe(25);
+  });
+
+  test('should convert square centimeter to millimeter test 1', () => {
+    expect(metric.squareCentimeterToMillimeter(4, 4)).toBe(1600);
+  });
+
+  test('should convert square centimeter to millimeter test 2', () => {
+    expect(metric.squareCentimeterToMillimeter(4)).toBe(1600);
+  });
+  
+  test('should convert square centimeter to meter test 1', () => {
+    expect(metric.squareCentimeterToMeter(4000, 4)).toBe(1.6);
+  });
+
+  test('should convert square centimeter to meter test 2', () => {
+    expect(metric.squareCentimeterToMeter(4000)).toBe(1600);
+  });
+  
+  test('should convert square centimeter to kilometer test 1', () => {
+    expect(metric.squareCentimeterToKilometer(40000, 4000)).toBe(1.6);
+  });
+  
+  test('should convert square centimeter to kilometer test 2', () => {
+    expect(metric.squareCentimeterToKilometer(40000)).toBe(16);
+  });
 });
 
-test('should convert square millimeter to centimeter test 1', () => {
-  expect(metric.squareMillimeterToCentimeter(40, 40)).toBe(16);
+// Meter to ...
+describe('Meter to ...', () => {
+  test('should convert square meter test 1', () => {
+    expect(metric.squareMeter(4, 4)).toBe(16);
+  });
+
+  test('should convert square meter test 1', () => {
+    expect(metric.squareMeter(4)).toBe(16);
+  });
+
+  test('should convert square meter to millimeter test 1', () => {
+    expect(metric.squareMeterToMillimeter(4, 4)).toBe(16000000);
+  });
+  
+  test('should convert square meter to millimeter test 2', () => {
+    expect(metric.squareMeterToMillimeter(4)).toBe(16000000);
+  });
+
+  test('should convert square meter to centimeter test 1', () => {
+    expect(metric.squareMeterToCentimeter(4, 4)).toBe(160000);
+  });
+  
+  test('should convert square meter to centimeter test 2', () => {
+    expect(metric.squareMeterToCentimeter(4)).toBe(160000);
+  });
+
+  test('should convert square meter to kilometer test 1', () => {
+    expect(metric.squareMeterToKilometer(4000, 4000)).toBe(16);
+  });
+  
+  test('should convert square meter to kilometer test 2', () => {
+    expect(metric.squareMeterToKilometer(4000)).toBe(16);
+  });
 });
 
-test('should convert square millimeter to centimeter test 2', () => {
-  expect(metric.squareMillimeterToCentimeter(40)).toBe(16);
-});
+describe('Kilometer to ...', () => {
+  test('should convert square kilometer test 1', () => {
+    expect(metric.squareKilometer(4, 4)).toBe(16);
+  });
 
-test('should convert square millimeter to meter', () => {
-  expect(metric.squareMillimeterToMeter(4000, 4000)).toBe(16);
-});
+  test('should convert square kilometer test 1', () => {
+    expect(metric.squareKilometer(4)).toBe(16);
+  });
 
-test('should convert square centimeter to millimeter', () => {
-  expect(metric.squareCentimeterToMillimeter(4, 4)).toBe(1600);
+  test('should convert square kilometer to millimeter test 1', () => {
+    expect(metric.squareKilometerToMillimeter(4, 4)).toBe(16000000000000);
+  });
+  
+  test('should convert square kilometer to millimeter test 2', () => {
+    expect(metric.squareKilometerToMillimeter(4)).toBe(16000000000000);
+  });
+
+  test('should convert square kilometer to centimeter test 1', () => {
+    expect(metric.squareKilometerToCentimeter(4, 4)).toBe(160000000000);
+  });
+  
+  test('should convert square kilometer to centimeter test 2', () => {
+    expect(metric.squareKilometerToCentimeter(4)).toBe(160000000000);
+  });
+
+  test('should convert square kilometer to meter test 1', () => {
+    expect(metric.squareKilometerToMeter(3, 5)).toBe(15000000);
+  });
+  
+  test('should convert square kilometer to meter test 2', () => {
+    expect(metric.squareKilometerToMeter(4)).toBe(16000000);
+  });
 });
